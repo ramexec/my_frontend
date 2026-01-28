c4d6c8fd520288345d32c85caaa56510be989729
RahulAnthony <twidlestick@gmail.com>
2026-01-28 18:46:38 +0530

    Updated data

--- src/components/Marquee-Animes/data.js
+++ src/components/Marquee-Animes/data.js
@@ -1,5 +1,9 @@
 export const images = [
     'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGhlanEwOXd1ajNiajVkcXZ1b2J4azZxOTg3aTdudXVxOXBvZ25qbiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/pVWuLuV1JESZJdebkI/giphy.gif',
     'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGhlanEwOXd1ajNiajVkcXZ1b2J4azZxOTg3aTdudXVxOXBvZ25qbiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/64VjZWm6RZGUmn2PKF/giphy.gif',
-    'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGhlanEwOXd1ajNiajVkcXZ1b2J4azZxOTg3aTdudXVxOXBvZ25qbiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/dKBES1ypGwZdyFQBQ7/giphy.gif'
+    'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGhlanEwOXd1ajNiajVkcXZ1b2J4azZxOTg3aTdudXVxOXBvZ25qbiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/dKBES1ypGwZdyFQBQ7/giphy.gif',
+    'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3I4bmo0N3MwajQybXZkOHJncHFnNG1oZGd4YW85NHAzcnY5MXBtZSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/qb1eHxhUHLdsc/giphy.gif',
+    'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3I4bmo0N3MwajQybXZkOHJncHFnNG1oZGd4YW85NHAzcnY5MXBtZSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/ZtB2l3jHiJsFa/giphy.gif',
+    'https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3cGQwMzM1ZWZpM2FkZHAydWE0bXM5djlpaW9hNGhlczRtNXg2ZWVmMSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/V216rdoG6TUVvXA0Tb/giphy.gif',
+    'https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3cGQwMzM1ZWZpM2FkZHAydWE0bXM5djlpaW9hNGhlczRtNXg2ZWVmMSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/FWAcpJsFT9mvrv0e7a/giphy.gif'
 ]
